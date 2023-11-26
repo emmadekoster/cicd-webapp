1e1f3113e0139d62147bbddeae2ba56f4dc5e4ba
Emma De Koster <emma.dekoster@student.hogent.be>
2023-11-27 00:07:22 +0100

    aanpassingen linting

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import Boom from "@hapi/boom";
-import morgan from "morgan";
-import persistence from "./persistence/index.js";
+import express from 'express';
+import Boom from '@hapi/boom';
+import morgan from 'morgan';
+import persistence from './persistence/index.js';
 
 const PORT = 3000;
 
@@ -13,21 +13,21 @@ function asyncMiddleware(fn) {
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 
 app.use((_, res, next) => {
-  res.set("X-Database-Used", process.env.MONGO_URL ? "MongoDB" : "SQLite");
+  res.set('X-Database-Used', process.env.MONGO_URL ? 'MongoDB' : 'SQLite');
   next();
 });
 
 // Add a route handler for the root URL ("/")
-app.get("/", (_, res) => {
-  res.send("Hello, World!");
+app.get('/', (_, res) => {
+  res.send('Hello, World!');
 });
 
 app.get(
-  "/animals",
+  '/animals',
   asyncMiddleware(async (_, res) => {
     const animals = await persistence.getAnimals();
     res.json(animals);
@@ -35,7 +35,7 @@ app.get(
 );
 
 app.get(
-  "/animals/:id",
+  '/animals/:id',
   asyncMiddleware(async (req, res) => {
     const animal = await persistence.getAnimal(Number(req.params.id));
     res.json(animal);
@@ -57,7 +57,7 @@ persistence
     });
   })
   .catch((err) => {
-    console.error("Database failed to connect, check the error below");
+    console.error('Database failed to connect, check the error below');
     console.error(err);
     process.exit(1);
   });
